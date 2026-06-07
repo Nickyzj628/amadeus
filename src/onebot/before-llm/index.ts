a22d8c7c3cf5a126e8c60d2ec72e7cf8927ba05c
RoxyZZZ <1927139831@qq.com>
2026-06-07 15:07:48 +0800

    feat(before-llm): 添加10秒节流

--- src/onebot/before-llm/index.ts
+++ src/onebot/before-llm/index.ts
@@ -6,11 +6,19 @@ import {
 	videoDetailToSegments,
 } from "./bililink.js";
 
+let lastHandleDate = 0;
+
 /**
  * 调用大模型之前的生命周期，如果消息无需模型处理，则返回要回复的消息段
  * @remarks 保证返回 Segment 数组，不抛异常
  */
 export const beforeLLM = async (e: GroupMessageEvent): Promise<Segment[]> => {
+  // 10秒节流
+  if (Date.now() - lastHandleDate <= 10000) {
+    return [];
+  }
+  lastHandleDate = Date.now();
+
 	const stringifiedSegmentData = JSON.stringify(
 		e.message.map((segment) => segment.data),
 	);
